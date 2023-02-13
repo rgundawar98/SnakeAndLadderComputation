2746d9aaf224c4b839d628ef541612ae5b9c6a6a
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-02-13 16:20:00 +0530

    Adding welcome message

---
+++ SnakeAndLadderCompuatation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakeAndLadderCompuatation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the snake and Ladder Computation Program");
+        }
+    }
+}
