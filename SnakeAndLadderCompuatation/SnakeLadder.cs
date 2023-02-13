b3b2301db228f4e40c43a1890baf22f991ea267b
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-02-13 17:27:56 +0530

    Checking player reached at position 100 UC4_PlayerWinningPosition100

--- SnakeAndLadderCompuatation/SnakeLadder.cs
+++ SnakeAndLadderCompuatation/SnakeLadder.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace SnakeAndLadderCompuatation
 {
@@ -12,29 +14,40 @@ namespace SnakeAndLadderCompuatation
         {
             int position = 0;
             string Player1 = "Rahul";
-            Console.WriteLine("Player name is {0} and its position is {1}",Player1,position);
-            Random random= new Random();
-            int RollDie =random.Next(1,7);
-            Console.WriteLine("The player rolled die and possibility as:"+RollDie);
-            Random random1= new Random();
-            int option= random1.Next(1,4);
-            switch(option)
+            Console.WriteLine("Player name is {0} and its position is {1}", Player1, position);
+            while (position < 100)
             {
-                case 1:
-                    Console.WriteLine("No Play");
-                    position = 0;
-                    Console.WriteLine("After getting No play the player position:{0}",position);
-                    break;
-                case 2:
-                    Console.WriteLine("Getting Ladder");
-                    position = position + RollDie; 
-                    Console.WriteLine("After getting ladder player moves ahead by the nnumber of position received in the die:"+position);
-                    break;
-                case 3:
-                    Console.WriteLine("Snake Bite");
-                    position =RollDie - position;
-                    Console.WriteLine("After getting Snake bite player moves behind by the number of position he received on die:"+position);
-                    break;
+            Random random = new Random();
+            int RollDie = random.Next(1, 7);
+            Console.WriteLine("Random number:"+RollDie);
+            Random random1 = new Random();
+            int option = random1.Next(1, 4);
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("No Play");
+                        position = 0;
+                        Console.WriteLine("After getting No play the player position:{0}", position);
+                        break;
+                    case 2:
+                        Console.WriteLine("Getting Ladder");
+                        position += RollDie;
+                        Console.WriteLine("After getting ladder player moves ahead by the nnumber of position received in the die:" + position);
+                        break;
+                    case 3:
+                        Console.WriteLine("Snake Bite");
+                        position -= RollDie;
+                        if (position <= 0)
+                        {
+                            position = 0;
+                            Console.WriteLine("After getting snake and Position less than zero then Player Position: " + position);
+                        }
+                        else
+                        {
+                            Console.WriteLine("After getting snake Player Position: " + position);
+                        }
+                        break;
+                }
             }
         }
     }
