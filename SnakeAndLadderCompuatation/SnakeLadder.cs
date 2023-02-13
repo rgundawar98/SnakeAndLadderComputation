44b7e1a1b6c0f804cc023e7525566f757a9676f8
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-02-13 17:07:33 +0530

    Checking the option for Noplay,GettingLadder,SnakeBite UC3_CheckOption

--- SnakeAndLadderCompuatation/SnakeLadder.cs
+++ SnakeAndLadderCompuatation/SnakeLadder.cs
@@ -16,6 +16,26 @@ namespace SnakeAndLadderCompuatation
             Random random= new Random();
             int RollDie =random.Next(1,7);
             Console.WriteLine("The player rolled die and possibility as:"+RollDie);
+            Random random1= new Random();
+            int option= random1.Next(1,4);
+            switch(option)
+            {
+                case 1:
+                    Console.WriteLine("No Play");
+                    position = 0;
+                    Console.WriteLine("After getting No play the player position:{0}",position);
+                    break;
+                case 2:
+                    Console.WriteLine("Getting Ladder");
+                    position = position + RollDie; 
+                    Console.WriteLine("After getting ladder player moves ahead by the nnumber of position received in the die:"+position);
+                    break;
+                case 3:
+                    Console.WriteLine("Snake Bite");
+                    position =RollDie - position;
+                    Console.WriteLine("After getting Snake bite player moves behind by the number of position he received on die:"+position);
+                    break;
+            }
         }
     }
 }
