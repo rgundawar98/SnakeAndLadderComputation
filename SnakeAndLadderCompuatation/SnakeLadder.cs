027b15aafc5c4162cfa008eb1f22179e05dd586f
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-02-13 16:45:10 +0530

    Player rolled dice and he gets possibilities UC2_RollingDice

--- SnakeAndLadderCompuatation/SnakeLadder.cs
+++ SnakeAndLadderCompuatation/SnakeLadder.cs
@@ -13,6 +13,9 @@ namespace SnakeAndLadderCompuatation
             int position = 0;
             string Player1 = "Rahul";
             Console.WriteLine("Player name is {0} and its position is {1}",Player1,position);
+            Random random= new Random();
+            int RollDie =random.Next(1,7);
+            Console.WriteLine("The player rolled die and possibility as:"+RollDie);
         }
     }
 }
